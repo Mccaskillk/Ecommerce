239288a3f5f886ed2a13ab1f72f0f7992ea4715b
Kenneth McCaskill <kennethmccaskill@Kenneths-MacBook-Pro.local>
2022-11-01 22:47:44 -0500

    models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,14 +10,47 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+// Validate
+validate: {
+  isDecimal: true
+}
+},
+stock: {
+type: DataTypes.INTEGER,
+allowNull: false,
+defaultValue: 10,
+validate: {
+  isNumeric: true,
+}
+},
+category_id: {
+type: DataTypes.INTEGER,
+references: {
+  model: 'category',
+  key: 'id'
+}
+}
+},
+{
+sequelize,
+timestamps: false,
+freezeTableName: true,
+underscored: true,
+modelName: 'product',
+}
 );
 
 module.exports = Product;
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,16 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'product_id'
+});
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+Product,
+Category,
+Tag,
+ProductTag,
 };
